42e1e9789d4e887cb81513a0de6d5f6f2f1d7715
hagmann.io <info@hagmann.io>
2018-03-06 17:08:59 +0100

    update

--- page.php
+++ page.php
@@ -4,8 +4,6 @@
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
     <h2><?php the_title(); ?></h2>
 
-    <?php the_post_thumbnail(); ?>
-
     <div class="entry">
         <?php the_content(); ?>
     </div>
